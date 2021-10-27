6136acb7702542b95672ffe7dbe5add7fd51f97e
IronNand <eggersnd@etsu.edu>
2021-10-27 11:26:20 -0400

    add actions for registering

--- ISCTaxonomies/Taxonomies.php
+++ ISCTaxonomies/Taxonomies.php
@@ -10,10 +10,24 @@
  //ISC Capstone Group includes: 
  mainbody();
  function mainbody(){
-   
+   add_action('init', 'register_isc_posts');
+   add_action('init', 'register_isc_taxonomies');
  }
  function register_isc_posts(){
+   /*supports in register_post_type includes
    
+    ‘title’
+    ‘editor’ (content)
+    ‘author’
+    ‘thumbnail’ (featured image) (current theme must also support Post Thumbnails)
+    ‘excerpt’
+    ‘trackbacks’
+    ‘custom-fields’ (see Custom_Fields, aka meta-data)
+    ‘comments’ (also will see comment count balloon on edit screen)
+    ‘revisions’ (will store revisions)
+    ‘page-attributes’ (template and menu order) (hierarchical must be true)
+    ‘post-formats’ (see Post_Formats)
+    */
  }
  function register_isc_taxonomies(){
    
